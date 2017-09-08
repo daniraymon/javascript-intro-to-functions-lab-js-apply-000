fa0c3c9172e75622fd6cdb342c49c043768dad43
Daniel Raymon <daniraymon@gmail.com>
2017-09-08 22:40:23 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,6 @@
 
 describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function(string) {
+  it('receives one argument and returns it in all caps', function shout(string) {
     return string.toUpperCase();
   })
 })
